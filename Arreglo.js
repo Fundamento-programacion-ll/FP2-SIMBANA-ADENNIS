fe8addc1002d85fe5fbc11e657d987d2f0d3577d
Paul Simbaña <59964221+PaulSimbana10@users.noreply.github.com>
2020-02-12 09:24:10 -0500

    arreglos
    Js

---
+++ Arreglo.js
@@ -0,0 +1,88 @@
+ var arreglo = [1,2,3,4,5];
+console.log(arreglo.length);
+// //arreglo.pop(6);
+// //for (let index = 0; index < arreglo.length; index++) {
+//    // const element = arreglo[index];
+//   //  console.log(element);
+// //}
+
+
+// //console.log(arreglo.shift());
+// console.log(arreglo.reverse());
+// var palabraString = "paul";
+// var arregloPalabra = palabraString.split('');
+// console.log(arreglo.shift());
+// console.log(arreglo.unshift());
+
+
+
+// console.log(arregloPalabra.reverse());
+
+// console.log(arreglo);
+// var arregloDividido = arreglo.slice(2,4);
+// console.log(arregloDividido);
+
+// arreglo.splice(5,1);
+// console.log(arreglo);
+
+// arreglo.forEach((elemento, indice) => {
+
+//     console.log(elemento);
+   
+// });
+
+// var arregloMutado=arreglo.map((elemento,indice)=>{return elemento*34;}).forEach((element, indice)=>{console.log(indice)}).map((element)=>{return element-33});
+// console.log(arregloMutado);
+
+// var MetodoSome=arreglo.every((e,i)=>{
+//     return e == 2;
+
+// });
+
+// console.log(MetodoSome);
+
+// var arregloX2 = arreglo.forEach((Element,indice)=>{
+//     return Element*2
+    
+// }).some((e)=>{
+// return e% 2==0;
+
+
+// });
+
+// console.log(arregloX2);
+
+// var metodoFilter = arreglo.filter((Element, index)=>{
+
+//     return Element == 2 ;
+// });
+
+// console.log(metodoFilter);var edad=
+
+var edad ='';
+var arregloJson = [
+    {
+        Json1={
+            nombre = 'paul',
+            edad = 20
+        },
+        Json2={nombre = 'vane',
+        edad = 20},
+        Json3={nombre = 'ale',
+        edad = 33},
+        Json4={nombre = 'tefa',
+        edad = 19},
+        Json5={nombre = 'bryan',
+        edad = 21},
+
+    }
+]
+
+arregloJson.forEach((eleme, i)=>{
+
+     a += eleme.edad;
+
+
+});
+console.log(arregloJson);
+
